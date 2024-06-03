383f301ee41ba3430f19a4c680b57cf8828953f5
Juan José Medina Clavijo <jjr20060326@gmail.com>
2024-06-03 18:35:53 -0500

    se corrigio un pequeño bug y se añadio un mensaje de salida registrada correctamente

--- src/minero/minero.controller.ts
+++ src/minero/minero.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, HttpStatus, ParseIntPipe, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpStatus, ParseIntPipe, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { MineroService } from './minero.service';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { User } from '@prisma/client';
 
 @Controller('minero')
@@ -12,11 +12,12 @@ export class MineroController {
     @UseGuards(JwtAuthGuard)
     async registrarSalida(
         @Body("cedula", new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) cedula: number,
-        @Req() req: Request
+        @Req() req: Request,
+        @Res() res: Response
     ): Promise<void> {
         const userId = (req.user as User).id;
         await this.mineroService.registrarSalida(cedula, userId);
-
+        res.status(HttpStatus.OK).json({ message: 'Salida registrada correctamente' });
 
     }
 }
